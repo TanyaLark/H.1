5e1a0e8872fa8c7e96b9b91ca4cd1d15c6cce560
TanyaLark <tanyaenlang9898@gmail.com>
2020-11-27 17:19:07 +0200

    Date pickers code changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,21 @@
 document.addEventListener('DOMContentLoaded', async function () {
 
     let datePickers = document.querySelector('#date_pickers');
-    //Даты по умолчанию для datePickers
+    //Max date для datePickers
     let today = new Date();
     let todayYear = today.getUTCFullYear();
     let todayMonth = today.getUTCMonth() + 1;
     let todayDay = today.getUTCDate();
 
-    let monthAgo = new Date(todayYear, todayMonth - 1, todayDay + 1);
-    let monthAgoYear = monthAgo.getUTCFullYear();
-    let monthAgoMonth = monthAgo.getUTCMonth();
-    let monthAgoDay = monthAgo.getUTCDate();
-
     datePickers.innerHTML = `
             <!-- date picker1 -->
             <input class='my-4 mx-4' type="date" id="start_input" name="trip-start" 
-            value="${monthAgoYear}-${monthAgoMonth}-${monthAgoDay}"
-            min="2018-01-01" max="2020-12-31">
+            max="${todayYear}-${todayMonth}-${todayDay}">
 
             <!-- date picker2 -->
             <input class='my-4 mx-4 ' type="date" id="finish_input" name="trip-start"
-            value="${todayYear}-${todayMonth}-${todayDay}"
-            min="2018-01-01" max="2020-12-31">
+            max="${todayYear}-${todayMonth}-${todayDay}">
         `;
-
 
     let newsPlaceTag = document.querySelector('#news-list-place');
     let oneNewsPlaceTag = document.querySelector('#one-news-place');
@@ -32,25 +24,33 @@ document.addEventListener('DOMContentLoaded', async function () {
     backButton.classList.add('d-none');
 
     //Search query
+    let searchQuery = document.querySelector('#search_query');
+    let show = document.querySelector('#button_show');
     const buttonShow = document.getElementById('button_show');
+
     buttonShow.addEventListener('click', async () => {
-        search = 'q=' + document.getElementById('search_query').value;
+        let search = 'q=' + document.getElementById('search_query').value;
         if (search === 'q=') {
             search = 'q=everything'
         }
         //Получение значений дат из Date Pickers
         const datePicker1Value = document.getElementById('start_input');
         const datePicker2Value = document.getElementById('finish_input');
-        let inputValueDatePickers = '&from=' + datePicker1Value.value + '&to=' + datePicker2Value.value;// даты из DatePickers в формате 2020-10-07
-        console.log(inputValueDatePickers);
-        getURL(search, inputValueDatePickers);
 
+        let startDate = new Date(datePicker1Value.value);
+        let finishDate = new Date(datePicker2Value.value);
+        if(startDate > finishDate){
+            alert('Временной диапазон задан не корректно.');
+        } 
+
+        let inputValueDatePickers = '&from=' + datePicker1Value.value + '&to=' + datePicker2Value.value;// даты из DatePickers в формате 2020-10-07
+        getURL(search, inputValueDatePickers);
     });
 
     getURL();
 
     async function getURL(q = 'q=everything', fromTo = '') {
-        URL = `https://newsapi.org/v2/everything?${q}${fromTo}&pageSize=32&apiKey=dbb88b39eac14bff9fa8ac9af1cf4b1e`;
+        URL = `https://newsapi.org/v2/everything?${q}${fromTo}&pageSize=8&apiKey=dbb88b39eac14bff9fa8ac9af1cf4b1e`;
 
         let news = await fetch(URL);
         news = await news.json();
@@ -98,7 +98,9 @@ document.addEventListener('DOMContentLoaded', async function () {
                 oneNewsPlaceTag.classList.remove('d-none');
                 newsPlaceTag.classList.add('d-none');
                 backButton.classList.remove('d-none');
-
+                datePickers.classList.add('d-none');
+                searchQuery.classList.add('d-none');
+                show.classList.add('d-none');
             });
         }
 
@@ -106,6 +108,9 @@ document.addEventListener('DOMContentLoaded', async function () {
             oneNewsPlaceTag.classList.add('d-none');
             newsPlaceTag.classList.remove('d-none');
             backButton.classList.add('d-none');
+            datePickers.classList.remove('d-none');
+            searchQuery.classList.remove('d-none');
+            show.classList.remove('d-none');
         });
     }
 
